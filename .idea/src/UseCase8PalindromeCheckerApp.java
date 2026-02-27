0c0a36d4ac190581ba4f2a21dbf4a777487cc5e5
neerugantimourya-sketch <neerugantimourya@gmail.com>
2026-02-27 10:50:29 +0530

    UC8: Linked list based palindrome check

---
+++ .idea/src/UseCase8PalindromeCheckerApp.java
@@ -0,0 +1,74 @@
+public class UseCase8PalindromeCheckerApp {
+
+    // Node class for Singly Linked List
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // UC8: Linked List Based Palindrome Checker
+        String word = "madam";
+
+        // Build linked list from string
+        Node head = null;
+        Node tail = null;
+
+        for (int i = 0; i < word.length(); i++) {
+            Node newNode = new Node(word.charAt(i));
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+
+        // Find middle using fast and slow pointer
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        Node prev = null;
+        Node current = slow;
+
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        // Compare first and second half
+        Node left = head;
+        Node right = prev;
+        boolean isPalindrome = true;
+
+        while (right != null) {
+            if (left.data != right.data) {
+                isPalindrome = false;
+                break;
+            }
+            left = left.next;
+            right = right.next;
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is not a Palindrome");
+        }
+    }
+}
